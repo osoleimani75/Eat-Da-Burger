5c955e6de05307fa5691eddc5674e93177b755d3
osoleimani75 <osoleimani75@gmail.com>
2019-04-04 14:45:27 -0700

    Fixed oops

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
-    connection.connect(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     // Set up MySQL connection parameters
     connection = mysql.createConnection({
